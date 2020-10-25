6dc01eddbd44917099328da2177875f59f16d408
Xabat <xabatplaza@opendeusto.es>
2020-10-25 19:20:25 +0100

    Arrested y Boss(a falta de workers)

--- ProyectoProg3/src/Classes/Arrested.java
+++ ProyectoProg3/src/Classes/Arrested.java
@@ -1,5 +1,36 @@
 package Classes;
 
+import java.util.Date;
+
 public class Arrested extends Detained{
+	public static int counter = 1;
+	private String name_a;
+	private String lastName_a;
+	private Date age_a;
+	private String description;
+	public Arrested(String name, String lastName, int age, String name_a, String lastName_a, Date age_a,
+			String description) {
+		super(name, lastName, age);
+		this.counter = counter++;
+		this.name_a = name_a;
+		this.lastName_a = lastName_a;
+		this.age_a = age_a;
+		this.description = description;
+	}
+	public Arrested() {
+		super();
+		this.name_a = "";
+		this.lastName_a = "";
+		this.age_a = new Date();
+		this.description = "";
+	}
+	@Override
+	public String toString() {
+		return "Arrested name: " + name_a + ". Last name: " + lastName_a + ". Age: " + age_a + ". The description: "
+				+ description;
+	}
+	
+	
+	
 
 }
--- ProyectoProg3/src/Classes/Boss.java
+++ ProyectoProg3/src/Classes/Boss.java
@@ -1,5 +1,9 @@
 package Classes;
 
 public class Boss extends Workers{
+	protected String name_b;
+	protected String dni_b;
+	protected int age_b;
+	
 
 }
