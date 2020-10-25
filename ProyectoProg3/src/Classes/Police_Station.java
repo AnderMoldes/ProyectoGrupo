d0da61864ac398a495b1fd729f6a9f596a91ccc1
juan <juan.sanz@opendeusto.es>
2020-10-25 17:57:37 +0100

    syso

--- ProyectoProg3/src/Classes/Police_Station.java
+++ ProyectoProg3/src/Classes/Police_Station.java
@@ -4,5 +4,9 @@ public class Police_Station {
 	protected String name;
 	protected int workers;
 	protected String b;
+	
+	public static void main(String[] args) {
+		System.out.println("Xabat");
+	}
 
 }
