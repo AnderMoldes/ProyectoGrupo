d13d5458ffdceec4b5e22fcb48f66051ebdc04c1
Ander Moldes <72606664+AnderMoldes@users.noreply.github.com>
2020-11-03 19:39:35 +0100

    corrección de algunos tests

--- ProyectoProg3/src/Tests/Grade_1Test.java
+++ ProyectoProg3/src/Tests/Grade_1Test.java
@@ -2,50 +2,53 @@ package Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.Date;
+
 import org.junit.jupiter.api.Test;
 
 import Classes.Grade_1;
+import Classes.Specialty;
 
 public class Grade_1Test {
 
 	@Test
 	public void testGetName() {
-		Grade_1 g = new Grade_1(1, "Pedro", "Ortiz", "male", "arrests", 4, 5);
+		Grade_1 g = new Grade_1(1, "Pedro", "Ortiz", "male", Specialty.CONDUCTOR, new java.util.Date(), 4);
 		assertEquals("Pedro", g.getName());
 
 	}
 
 	@Test
 	public void testGetSurName() {
-		Grade_1 g = new Grade_1(1, "Pedro", "Ortiz", "male", "arrests", 4, 5);
+		Grade_1 g = new Grade_1(1, "Pedro", "Ortiz", "male", Specialty.CONDUCTOR, new java.util.Date(), 4);
 		assertEquals("Ortiz", g.getSurname());
 
 	}
 
 	@Test
 	public void testGetGender() {
-		Grade_1 g = new Grade_1(1, "Pedro", "Ortiz", "male", "arrests", 4, 5);
+		Grade_1 g = new Grade_1(1, "Pedro", "Ortiz", "male", Specialty.CONDUCTOR, new java.util.Date(), 4);
 		assertEquals("male", g.getGender());
 
 	}
 
 	@Test
 	public void testGetSpecialty() {
-		Grade_1 g = new Grade_1(1, "Pedro", "Ortiz", "male", "arrests", 4, 5);
+		Grade_1 g = new Grade_1(1, "Pedro", "Ortiz", "male", Specialty.CONDUCTOR, new java.util.Date(), 4);
 		assertEquals("arrests", g.getSpecialty());
 
 	}
 
 	@Test
 	public void testGetWorkingYears() {
-		Grade_1 g = new Grade_1(1, "Pedro", "Ortiz", "male", "arrests", 4, 5);
+		Grade_1 g = new Grade_1(1, "Pedro", "Ortiz", "male", Specialty.CONDUCTOR, new java.util.Date(), 4);
 		assertEquals(4, g.getWorkingYears());
 
 	}
 
 	@Test
 	public void testGetAssesment() {
-		Grade_1 g = new Grade_1(1, "Pedro", "Ortiz", "male", "arrests", 4, 5);
+		Grade_1 g = new Grade_1(1, "Pedro", "Ortiz", "male", Specialty.CONDUCTOR, new java.util.Date(), 4);
 		assertEquals(5, g.getAssessment());
 
 	}
