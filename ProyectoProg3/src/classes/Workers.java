c5467ce856725598a75733e153a3b74a4fd0a269
amold <amold@LAPTOP-VB1PSOLN.home>
2020-11-17 10:27:33 +0100

    cambios en la clase workers

--- ProyectoProg3/src/classes/Workers.java
+++ ProyectoProg3/src/classes/Workers.java
@@ -1,58 +1,119 @@
 package classes;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Workers implements Serializable{
     public static int counter = 0;
 
     protected int code;
+    protected int grade;
     protected String name;
     protected String surname;
     protected String gender;
+    protected String Specialty;
+    protected Date startWorkingIn;
+    protected String Assesment;
     
-	public Workers(int code, String name, String surname, String gender) {
+	
+	
+	public Workers(int code, int grade, String name, String surname, String gender, String specialty,
+			Date startWorkingIn, String assesment) {
 		super();
 		this.code = counter++;
+		this.grade = grade;
 		this.name = name;
 		this.surname = surname;
 		this.gender = gender;
+		this.Specialty = specialty;
+		this.startWorkingIn = startWorkingIn;
+		Assesment = assesment;
 	}
+	
 	public Workers() {
 		super();
-		this.code = counter++;
+		this.grade = 0;
 		this.name = "";
 		this.surname = "";
 		this.gender = "";
+		this.Specialty = "";
+		this.startWorkingIn = null;
+		this.Assesment = "";
 	}
+
 	
+
 	public int getCode() {
-		return code;
+		return counter++;
 	}
-	public void setCode(int code) {
-		Workers.counter = counter;
+
+
+	public int getGrade() {
+		return grade;
 	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getSurname() {
 		return surname;
 	}
+
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
+	public String getSpecialty() {
+		return Specialty;
+	}
+
+	public void setSpecialty(String specialty) {
+		Specialty = specialty;
+	}
+
+	public Date getStartWorkingIn() {
+		return startWorkingIn;
+	}
+
+	public void setStartWorkingIn(Date startWorkingIn) {
+		this.startWorkingIn = startWorkingIn;
+	}
+
+	public String getAssesment() {
+		return Assesment;
+	}
+
+	public void setAssesment(String assesment) {
+		Assesment = assesment;
+	}
+
 	@Override
 	public String toString() {
-		return "Workers number " + code + ". Name: " + name + " " + surname + ". Gender: " + gender ;
+		return "Workers [code=" + code + ", grade=" + grade + ", name=" + name + ", surname=" + surname + ", gender="
+				+ gender + ", Specialty=" + Specialty + ", startWorkingIn=" + startWorkingIn + ", Assesment="
+				+ Assesment + "]";
 	}
+	
+	
+	
+	
     
 
 }
