40b67fc5abc01b926a22107ea6feb1a59c342954
Diego <diegoburzaco@opendeusto.es>
2021-01-17 17:05:50 +0100

    teses

--- ProyectoProg3/test/tests/GeneradorMatriculasTest.java
+++ ProyectoProg3/test/tests/GeneradorMatriculasTest.java
@@ -1,5 +1,25 @@
 package tests;
 
-public class GeneradorMatriculasTest {
+import org.junit.jupiter.api.Test;
 
+import classes.GeneradorMatriculasV;
+
+public class GeneradorMatriculasTest {
+	
+	@Test
+	public void tesGetRandom() {
+		GeneradorMatriculasV gm = new GeneradorMatriculasV();
+	}
+	@Test
+	public void tesGetPlate() {
+		GeneradorMatriculasV gm = new GeneradorMatriculasV();
+	}
+	@Test
+	public void tesGetPosition() {
+		GeneradorMatriculasV gm = new GeneradorMatriculasV();
+	}
+	@Test
+	public void tesGetLongg() {
+		GeneradorMatriculasV gm = new GeneradorMatriculasV();
+	}
 }
