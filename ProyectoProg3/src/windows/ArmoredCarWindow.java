54e4cc4ffce62ba9bf779bb79807d8628e7c5b78
Diego <diegoburzaco@opendeusto.es>
2021-01-17 22:04:54 +0100

    retoques

--- ProyectoProg3/src/windows/ArmoredCarWindow.java
+++ ProyectoProg3/src/windows/ArmoredCarWindow.java
@@ -13,28 +13,24 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-public class ArmoredCarWindow extends JFrame{
-
+public class ArmoredCarWindow extends JFrame {
+	//Declaramos
 	JPanel panelPhoto;
 	JPanel panelText;
-	
 	JLabel image;
-	
 	JTextArea text;
 	JButton back;
-	
-    
-	
-	public ArmoredCarWindow(){
+
+	public ArmoredCarWindow() {
 		JPanel panelPhoto = new JPanel();
 		JPanel panelText = new JPanel();
-		
+
 		image = new JLabel(new ImageIcon("armoredcar.jpg"));
-		
+
 		JTextArea text = new JTextArea();
-		
-		JButton back = new JButton("Back");	
-		
+
+		JButton back = new JButton("Back");
+
 		text.setEditable(false);
 		text.setText(" The armored car is a wheeled armored personnel carrier designed for military\n"
 				+ " and law enforcement use. It is in use by several military forces and law enforcement\n"
@@ -45,37 +41,38 @@ public class ArmoredCarWindow extends JFrame{
 				+ " way during terrorist threats, hostage incidents, or encounters with large gatherings\n"
 				+ " of aggressors. The armored car is designed to provide protection from a variety\n"
 				+ " of small arms, explosives and IED threats.");
-		
+		//Eventos
 		back.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				dispose();
-				
+
 			}
 		});
 		
-		this.setLayout(new GridLayout(2,1));
+		//Diseño
+		this.setLayout(new GridLayout(2, 1));
 		panelPhoto.setLayout(new BorderLayout());
-		
+
 		panelPhoto.add(image, BorderLayout.CENTER);
-		
+
 		add(panelPhoto);
 		add(panelText);
-		
+
 		panelPhoto.setLayout(new FlowLayout());
-		
+
 		panelText.setLayout(new BorderLayout());
-		
+
 		panelText.add(text, BorderLayout.CENTER);
 		panelText.add(back, BorderLayout.SOUTH);
-		
+
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setTitle("ARMORED CAR");	
+		setTitle("ARMORED CAR");
 		pack();
 		setResizable(false);
 		setVisible(true);
-		setLocationRelativeTo( null );
-}
-	
+		setLocationRelativeTo(null);
+	}
+
 }
