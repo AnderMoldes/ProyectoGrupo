a91b1778fc674388261878e818c94778c962aba7
Diego <diegoburzaco@opendeusto.es>
2021-01-17 16:52:00 +0100

    test order

--- ProyectoProg3/test/tests/OrderTest.java
+++ ProyectoProg3/test/tests/OrderTest.java
@@ -1,5 +1,28 @@
 package tests;
 
+import org.junit.Test;
+
+import classes.Order;
+
 public class OrderTest {
 
+	@Test
+	public void testGetTable() {
+		Order o = new Order();
+		//asserEquals();
+	}
+
+	@Test
+	public void testGetIzq() {
+		Order o = new Order();
+		//asserEquals();
+	}
+
+	@Test
+	public void testGetDer() {
+		Order o = new Order();
+		//asserEquals();
+	}
 }
+
+//
